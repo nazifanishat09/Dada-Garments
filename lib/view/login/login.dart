19b0c7a80a222d0d18993ec26081d0b8b2e13770
nazifanishat09 <nazifanishat2024@gmail.com>
2026-01-16 02:12:23 +0600

    tab bar

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../widget/custom_appbar.dart';
 
+
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
 
@@ -12,10 +13,32 @@ class LogInScreen extends StatefulWidget {
 class _LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-
+    return DefaultTabController(length: 2,
+      child: Scaffold(appBar: CustomAppBar(),
+      body: Column(
+        children: [
+        const TabBar(
+        labelColor: Colors.black,
+        unselectedLabelColor: Colors.grey,
+        indicatorColor: Colors.orange,  // underline color
+        indicatorWeight: 3,             // underline thickness
+        indicatorSize: TabBarIndicatorSize.tab, // text অনুযায়ী underline
+        tabs: [
+          Tab(text: "Sign In"),
+          Tab(text: "Sign Up"),
+        ],
       ),
-    );
+      const Expanded(
+        child: TabBarView(
+          children: [
+            Center(child: Text("Sign In Page")),
+            Center(child: Text("Sign Up Page")),
+          ],
+        ),
+      ),
+      ],
+    ),
+
+    ));
   }
 }
